d707ac03232b70739f636adf21b3adfef27a1312
MarcoBrunoBR <marco.bruno.br@gmail.com>
2017-08-24 01:42:45 -0300

    Implemented ajax with fetch

--- src/asset/js/modal.js
+++ src/asset/js/modal.js
@@ -44,9 +44,26 @@
 
     $modal.classList.add('modal_isCreating')
 
-    fetch('/url', {
+    const settingsTime = {
+      minute: 3, 
+      second: 0, 
+      srcLogo: 'https://imgur.com/download/kjWzqUd',
+      altLog: 'Logo do CollabCode',
+      urlSong: 'https://www.myinstants.com/media/sounds/buzina-trem-araponga-que-porrada-na-orelha.mp3',
+      startSong: false,
+      backgroundColor: '#FFFFFF', 
+      timeColor: '#F36A7E', 
+      processColor: '#C3C5C6', 
+      processBackgroundColor: '#F2F3F3'
+    }
+
+    fetch('http://localhost:3000/url', {
       method: 'POST',
-      bdoy: {foi: true}
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(settingsTime)
     })
   })
 
--- src/route/url.js
+++ src/route/url.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  console.log('body', req.body)
 })
 
 module.exports = router
